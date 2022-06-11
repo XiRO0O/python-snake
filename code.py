bce9094b49d9839eaffe7ca55e7268f6cc246f59
ONKJUNUA <jaseko2002@gmail.com>
2022-06-11 23:49:06 +0200

    Creating base of snake and fruit

--- code.py
+++ code.py
@@ -1,11 +1,35 @@
-import pygame, sys
+import pygame, sys, random
+from pygame.math import Vector2
+
+class SNAKE:
+    def __init__(self):
+        self.body = [Vector2(5,10),Vector2(6,10),Vector2(7,10)]
+    
+    def draw_snake(self):
+        for block in self.body:
+            x_pos = int(block.x * cell_size)
+            y_pos = int(block.y * cell_size)
+            block_rect = pygame.Rect(x_pos,y_pos,cell_size,cell_size)
+            pygame.draw.rect(screen,(250,0,0),block_rect)
+
+class FRUIT:
+    def __init__(self):
+        self.x = random.randint(0,cell_number - 1)
+        self.y = random.randint(0,cell_number - 1)
+        self.pos = Vector2(self.x,self.y)
+
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size),cell_size,cell_size)
+        pygame.draw.rect(screen,(126,166,114),fruit_rect)
 
 pygame.init()
-screen = pygame.display.set_mode((1280,720))
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number * cell_size,cell_number * cell_size))
 clock = pygame.time.Clock()
-test_surface = pygame.Surface((100,200))
-test_surface.fill((200,100,0))
-x_pos = 200
+
+snake = SNAKE()
+fruit = FRUIT()
 
 while True:
     for event in pygame.event.get():
@@ -13,8 +37,8 @@ while True:
             pygame.quit()
             sys.exit()
 
-    screen.fill((100,160,60))
-    x_pos += 1
-    screen.blit(test_surface,(x_pos,250))
+    screen.fill((77,77,77))
+    snake.draw_snake()
+    fruit.draw_fruit()
     pygame.display.update()
     clock.tick(60)
