e22a3055252a60038965b56a8d0e137f6501c2db
ONKJUNUA <jaseko2002@gmail.com>
2022-06-11 21:00:26 +0200

    Screen update

--- code.py
+++ code.py
@@ -1,8 +1,11 @@
-import pygame
+import pygame, sys
 
 pygame.init()
-screen = pygame.display.set_mode((400,500))
+screen = pygame.display.set_mode((1280,720))
 
 while True:
-
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
     pygame.display.update()
