eb699e3d7d58266582bd67ea042f2a52346496bc
ONKJUNUA <jaseko2002@gmail.com>
2022-06-12 00:02:50 +0200

    Moving snake

--- code.py
+++ code.py
@@ -4,6 +4,7 @@ from pygame.math import Vector2
 class SNAKE:
     def __init__(self):
         self.body = [Vector2(5,10),Vector2(6,10),Vector2(7,10)]
+        self.direction = Vector2(1,0)
     
     def draw_snake(self):
         for block in self.body:
@@ -11,6 +12,11 @@ class SNAKE:
             y_pos = int(block.y * cell_size)
             block_rect = pygame.Rect(x_pos,y_pos,cell_size,cell_size)
             pygame.draw.rect(screen,(250,0,0),block_rect)
+
+    def move_snake(self):
+        body_copy = self.body[:-1]
+        body_copy.insert(0,body_copy[0] + self.direction)
+        self.body = body_copy [:]
 
 class FRUIT:
     def __init__(self):
@@ -31,11 +37,25 @@ clock = pygame.time.Clock()
 snake = SNAKE()
 fruit = FRUIT()
 
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE,150)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == SCREEN_UPDATE:
+            snake.move_snake()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                snake.direction = Vector2(0,-1)
+            if event.key == pygame.K_DOWN:
+                snake.direction = Vector2(0,1)
+            if event.key == pygame.K_RIGHT:
+                snake.direction = Vector2(1,0)
+            if event.key == pygame.K_LEFT:
+                snake.direction = Vector2(-1,0)
 
     screen.fill((77,77,77))
     snake.draw_snake()
